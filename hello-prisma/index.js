b4a8211483c6078e909c500f7a6f19b9ff8a9dcc
jua <akshaykumarragavan@gmail.com>
2023-01-14 12:40:56 +0530

    middleware app.use

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
+
 const app = express();
 
 require("dotenv").config({
   path: `.env`,
   override: true,
 });
+
+// regular middleware
+app.use(express.json()); // parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // parses incoming requests with URL-encoded payloads
+app.use(express.static); // serves static assets such as HTML files, images, and so on.
+
+// cookie middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   console.log("req :", req.method, req.httpVersion, req.url);
