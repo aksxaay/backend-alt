e528996b829f825fae93f1d370e616c4cefe7146
jua <akshaykumarragavan@gmail.com>
2023-01-14 16:19:37 +0530

    bcrypt salted password

--- hello-prisma/controllers/userController.js
+++ hello-prisma/controllers/userController.js
@@ -2,6 +2,8 @@
 // we can create objects from prisma client
 const prisma = require("../prisma");
 const cookieToken = require("../utils/cookieToken");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 // user signup
 
@@ -13,18 +15,18 @@ exports.signup = async (req, res, next) => {
     // check
     if (!name || !email || !password) {
       throw new Error("Please provide the necessary fields");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+      // send user a token
+      cookieToken(user, res);
     }
-
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
-
-    // send user a token
-    cookieToken(user, res);
   } catch (error) {
     res.status(422).json({
       message: error.message,
