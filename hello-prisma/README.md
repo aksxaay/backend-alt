9b56ea92a419fee2a0fe52136ccbc8bc4fc520b6
jua <akshaykumarragavan@gmail.com>
2023-01-14 12:34:45 +0530

    simple express app

---
+++ hello-prisma/README.md
@@ -0,0 +1,6 @@
+## prisma setup (hello=prisma)
+```sh
+npm i bcryptjs cookie-parser dotenv express jsonwebtoken validator
+```
+
+don't know what any of them are for yet-
--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,2 +1,16 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const express = require("express");
+const app = express();
+
+require("dotenv").config({
+  path: `.env`,
+  override: true,
+});
+
+app.get("/", (req, res) => {
+  console.log("req :", req.method, req.httpVersion, req.url);
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
+});
