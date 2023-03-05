174994cbcfaae22560606ff27cbc071681820ba9
jua <akshaykumarragavan@gmail.com>
2023-03-05 19:47:36 +0530

    jwt example

--- hello-prisma/README.md
+++ hello-prisma/README.md
@@ -4,3 +4,9 @@ npm i bcryptjs cookie-parser dotenv express jsonwebtoken validator
 ```
 
 don't know what any of them are for yet-
+
+
+[twilio talk, by Randolph](https://www.youtube.com/watch?v=j8Yxff6L_po)
+
+
+added jwt token sample using the gfg example.
--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -29,4 +31,35 @@ app.get("/api", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on Port ${process.env.PORT}`);
+});
+
+// jwt (jot)
+app.post("/user/generateToken", (req, res) => {
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+    time: Date(),
+    userId: 69,
+  };
+
+  const token = jwt.sign(data, jwtSecretKey);
+  res.send(token);
+});
+
+app.get("/user/validateToken", (req, res) => {
+  // tokens are generally passed in the header of the request due to security
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const token = req.header(tokenHeaderKey);
+    const verified = jwt.verify(token, jwtSecretKey);
+
+    if (verified) {
+      res.send("Sucessfully Verified");
+    } else {
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    return res.status(401).send(error);
+  }
 });
